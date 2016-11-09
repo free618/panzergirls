145f03066fac65bd6cfe20c45cdc690a008e8f5b
free618 <freee618@126.com>
2016-11-09 18:33:19 +0800

    test update

---
+++ Android/update/21/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {["totalsize"]=1347355,["zipcount"]=1,["deletelist"]={},["totalfilecount"]=381,["version"]="21",["ziplist"]={["1.zip"]={["filesize"]=1404578,["filecount"]=381,["md5"]="70dc90d9564826b81500becce8bf7e59"}}}
+local md5 = "DCD4C2ADB2A7C15E83BCC62CD05F0D77";return info, md5
---
+++ newestversion.lua
@@ -0,0 +1,3 @@
+local newestversion = "21"
+local urls = {"https://github.com/free618/panzergirls/blob/master/",}
+return newestversion,urls
