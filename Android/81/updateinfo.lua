4b9045d23cc66ec3d07c87ef7c080459fb1feaf0
free618 <freee618@126.com>
2016-11-15 11:34:04 +0800

    new version 81

---
+++ Android/81/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='81', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1813438, ['totalfilecount']=8, ['ziplist']={['1.zip']={['filesize']=1813438, ['filecount']=8,['md5']='c93b55ed6748d9f78d8904a467e3d741'}}
+local md5 = "8b80bd80c79738e4c577ab48d565bb43";return info, md5
---
+++ IOS/81/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='81', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1878473, ['totalfilecount']=8, ['ziplist']={['1.zip']={['filesize']=1878473, ['filecount']=8,['md5']='22bb8455136a9f47bb7de8390ff65be3'}}
+local md5 = "ef17b5b986975b217fe586de8bc20f93";return info, md5
