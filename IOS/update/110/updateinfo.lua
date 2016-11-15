d40b38421a84476fa5dd0d51ab2b339761f1b2fe
free618 <freee618@126.com>
2016-11-15 11:35:10 +0800

    new version 81

--- IOS/update/110/updateinfo.lua
+++
@@ -1,2 +0,0 @@
-local info = {["totalsize"]=1471998,["zipcount"]=1,["deletelist"]={},["totalfilecount"]=381,["version"]="110",["ziplist"]={["1.zip"]={["filesize"]=1483184,["filecount"]=381,["md5"]="befa6794b82b2cdf5c2ed6b2a58777bd"}}}
-local md5 = "A356FA95310322AE3D55D7A4D5CC5B00";return info, md5
